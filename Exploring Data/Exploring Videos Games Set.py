1e8b6cdc008ec715f4d1ccf60c7fa0525bf7805c
rorymurphyza <rory@rorym.co.za>
2017-03-09 07:54:03 +1100

    Start of Visual Exploration

---
+++ Exploring Data/Exploring Videos Games Set.py
@@ -0,0 +1,19 @@
+A First Level Header
+====================
+
+A Second Level Header
+---------------------
+
+Now is the time for all good men to come to
+the aid of their country. This is just a
+regular paragraph.
+
+The quick brown fox jumped over the lazy
+dogs back.
+
+### Header 3
+
+This is a blockquote.
+```{python}
+print("Hello world")
+```
